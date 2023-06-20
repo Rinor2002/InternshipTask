6a1de50d37e08e9a0230a8b6d6b9ce66401d3cb6
Rinor2002 <rinorkastrati46@gmail.com>
2023-06-20 13:31:50 +0200

    created form to display options, along with the backButton to go back to step one

--- React/pabau/src/app/page.tsx
+++ React/pabau/src/app/page.tsx
@@ -1,95 +1,90 @@
+'use client'
 import Image from 'next/image'
-import styles from './page.module.css'
-
+import { useState } from 'react';
 export default function Home() {
+  const [currentStep, setCurrentStep] = useState('1/2');
+  const [selectedOption, setSelectedOption] = useState(null);
+  const handleOptionSelection = (optionName) => {
+    setCurrentStep('2/2');
+    setSelectedOption(optionName);
+  };
+
+  const handleBack = () => {
+    setCurrentStep('1/2');
+    setSelectedOption(null);
+  };
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+   <main>
+    <header>
+      <h2><strong>Choose Service</strong></h2>
+      <p>{currentStep}</p>
+    </header>
+    <section>
+      { selectedOption ? 
+      (
+      <form>
+        <button type="button" className='optionButton' onClick={handleBack}>
+          Back
+        </button>
+        <h2>Selected Option: {selectedOption}</h2>
+      </form>
+      ) 
+      : 
+      (
+      <form>
+        <button type="button" className='optionButton' onClick={() => handleOptionSelection('Dermal Fillers')}>
+          <div>
+            <Image 
+            src="/dermalFillers.jpeg"
+            alt="noImage"
+            width={30}
+            height={30} />
+            <span>Dermal Fillers</span>
+          </div>
+          <span>&gt;</span>
+        </button>
+        <button type="button" className='optionButton' onClick={() => handleOptionSelection('Secret RF')}>
+          <div>
+            <Image 
+            src="/secretRF.jpeg"
+            alt="noImage"
+            width={30}
+            height={30} />
+            <span>Secret RF</span>
+          </div>
+          <span>&gt;</span>
+        </button>
+        <button type="button" className='optionButton' onClick={() => handleOptionSelection('Facials')}>
+          <div>
+            <Image 
+            src="/facials.jpeg"
+            alt="noImage"
+            width={30}
+            height={30} />
+            <span>Facials</span>
+          </div>
+          <span>&gt;</span>
+        </button>
+        <button type="button" className='optionButton' onClick={() => handleOptionSelection('Fat Dissolve')}>
+          <div>
+            <Image 
+            src="/fatDiss.jpeg"
+            alt="noImage"
+            width={30}
+            height={30} />
+            <span>Fat Dissolve</span>
+          </div>
+          <span>&gt;</span>
+        </button>
+      </form>
+      )}
+      <span className='consultation'>
+        Not sure about consultation type? Please, call - <strong> 0203 7959063</strong>
+      </span>
+    </section>
+    <footer>
+      <span>Powered by Pabau</span>
+    </footer>
+   </main>
   )
 }
